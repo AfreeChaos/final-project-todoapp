4d48854efab3730bbc18fd5de1c172f8500464a7
Àfrica Calderón Alcaraz <africa@imac-de-africa.home>
2023-01-17 22:20:20 +0100

    17.01

--- ironhack-front-final-project-template-master/src/components/Home.vue
+++ ironhack-front-final-project-template-master/src/components/Home.vue
@@ -1,17 +1,50 @@
 <template>
   <section>
-    <div class="menu">
-        <img src="../assets/lista-de-quehaceres.png" alt="" style="width: 124px; margin-top: 80px; margin-bottom: 80px;">
-        <ul class="nav">
-            <router-link to="/home" class="linkselect">HOME</router-link>
-            <router-link to="/home" class="link">MEMBERS</router-link>
-            <router-link to="/home" class="link">CALENDAR</router-link>
-            <router-link to="/home" class="link">SETTINGS</router-link>
-            <hr class="link">
-            <p class="link">MY BOARDS</p>
-            <router-link to="/home" class="link">TASKS</router-link>
-        </ul>
+    <div>    
+        <div class="section1">
+            <nav class="menu">
+                <img src="../assets/lista-de-quehaceres.png" alt="" style="width: 124px; margin-top: 80px; margin-bottom: 80px;">
+                <ul class="nav">
+                    <router-link to="/home" class="linkselect">HOME</router-link>
+                    <router-link to="/home" class="link">MEMBERS</router-link>
+                    <router-link to="/home" class="link">CALENDAR</router-link>
+                    <router-link to="/home" class="link">SETTINGS</router-link>
+                    <hr class="link">
+                    <p class="link">MY BOARDS</p>
+                    <router-link to="/home" class="link">To-Do Project | Tasks</router-link>
+                </ul>
+            </nav>
+            <div>
+                <div>
+                    <div class="search-bar">
+                        <form class="form-inline">
+                            <input class="search-form" type="search" placeholder="Search..." aria-label="Search">
+                            <button class="search-btn" type="submit">Search</button>
+                        </form>
+                        <div>
+                            <img src="../assets/profile-pic1.avif" alt="" style="width: 40px; border-radius: 50%; margin-left: 16px;">
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <h1 class="title1">Private space</h1>
+                    <hr style="background-color: #DDDCE0; height: 1px; margin: 16px;">
+                </div>
+                <div>
+                    <h2 class="title2">My boards</h2>
+                    <div class="boards">
+                        <img src="../assets/wallpaper.jpeg" alt="">
+                        <div class="title3">
+                            <router-link to="/home" class="link">To-Do Project | Tasks</router-link>
+                            <img src="../assets/gold-star.png" alt="" style="width: 16px; height: 16px;">
+                        </div>
+                        <p>Description</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+
   </section>
 </template>
 
@@ -20,11 +53,13 @@
 </script>
 
 <style scoped>
-section {
+.section1 {
     display: flex;
+    flex-direction: row;
 }
 .menu {
-    width: 320px;
+    width: 280px;
+    height: 100vh;
     background-color: #FAECDA;
     border-radius: 0 32px 32px 0;
     display: flex;
@@ -49,5 +84,48 @@ hr {
 .link {
     margin-bottom: 24px;
     font-weight: 300;
+}
+.search-bar {
+    width: 100vh;
+    margin: 24px;
+    display: flex;
+    justify-content: space-between;
+}
+.search-form {
+    height: 40px;
+    width: 320px;
+    border: 1px #DDDCE0 solid;
+    border-radius: 8px;
+    padding: 20px;
+    margin-right: 8px;
+    font-weight: 300;
+}
+.search-btn {
+    width: 100px;
+    height: 40px;
+    border: 1px #4A749B solid;
+    border-radius: 8px;
+    color: #4A749B;
+    font-weight: 300;
+}
+.title1 {
+    margin: 24px;
+    font-size: 24px;
+    font-weight: 600;
+}
+.title2 {
+    margin: 24px;
+    font-size: 20px;
+    font-weight: 600;
+}
+.boards {
+    margin: 24px;
+    width: 320px;
+}
+.title3 {
+    display: flex;
+    align-content: center;
+    margin: 16px 16px 16px 0;
+    justify-content: space-between;
 }
 </style>
